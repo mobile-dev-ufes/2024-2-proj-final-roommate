517a3da63466c17368725b50c472de3d3b442c50
lcrodrigues0 <leticia.c.rodrigues0@gmail.com>
2025-03-06 18:28:25 -0300

    feat: Update login layout

--- app/src/main/java/com/example/roommate/ui/LoginActivity.kt
+++ app/src/main/java/com/example/roommate/ui/LoginActivity.kt
@@ -1,17 +1,11 @@
 package com.example.roommate.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.roommate.R
 import com.example.roommate.databinding.ActivityLoginBinding
-import com.example.roommate.databinding.ActivityMainBinding
-import com.example.roommate.databinding.ActivitySplashBinding
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
