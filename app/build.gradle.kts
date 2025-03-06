2c62368692e4d07b1974bfa96fb69948d1843784
lcrodrigues0 <leticia.c.rodrigues0@gmail.com>
2025-03-06 18:03:10 -0300

    feat: Create splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,10 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    buildFeatures {
+        viewBinding = true;
+    }
 }
 
 dependencies {
@@ -45,6 +49,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation (libs.androidx.core.splashscreen)
 
     implementation(libs.material)
 }
---
+++ app/src/main/java/com/example/roommate/ui/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.roommate.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.roommate.R
+import com.example.roommate.databinding.ActivityLoginBinding
+import com.example.roommate.databinding.ActivityMainBinding
+import com.example.roommate.databinding.ActivitySplashBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
