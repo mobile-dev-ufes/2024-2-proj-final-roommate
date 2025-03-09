cdbc3efa9e2e17d3930a7bcad2a1eee2e53cede7
lcrodrigues0 <leticia.c.rodrigues0@gmail.com>
2025-03-09 00:28:56 -0300

    feat: start toolbar

--- app/src/main/java/com/example/roommate/ui/activities/HomeActivity.kt
+++ app/src/main/java/com/example/roommate/ui/activities/HomeActivity.kt
@@ -1,7 +1,12 @@
 package com.example.roommate.ui.activities
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.roommate.R
 import com.example.roommate.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
@@ -11,5 +16,18 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setToolbar()
+    }
+
+    private fun setToolbar() {
+        val navHostFrag =
+            supportFragmentManager.findFragmentById(R.id.fragment_container_home) as NavHostFragment
+        val navController = navHostFrag.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        binding.toolbar.inflateMenu(R.menu.toolbar_menu)
+
     }
 }
