25943a6cd3bdd0e821a75de10b11cacf970cb86b
lcrodrigues0 <leticia.c.rodrigues0@gmail.com>
2025-03-13 11:30:55 -0300

    feat: define usermodel

--- app/src/main/java/com/example/roommate/data/model/UserModel.kt
+++ app/src/main/java/com/example/roommate/data/model/UserModel.kt
@@ -1,8 +1,19 @@
 package com.example.roommate.data.model
 
+import java.time.LocalDate
+
+enum class SexEnum {
+    MALE, FEMALE, OTHER, UNIDENTIFIED
+}
+
 class UserModel(
     var name: String,
-    var bio: String
+    var bio: String,
+    var age: Int?,
+    var sex: SexEnum?,
+    var birthDate: LocalDate?,
+    var photo_uri: String?
 ) {
 
+    constructor(name: String, bio: String) : this(name, bio, null, null, null, null)
 }
