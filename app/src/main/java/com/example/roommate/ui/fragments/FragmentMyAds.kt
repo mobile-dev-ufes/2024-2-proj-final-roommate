5d37505ceb5e3c4d4c76c755d05e9f841881b0e4
lcrodrigues0 <leticia.c.rodrigues0@gmail.com>
2025-03-13 03:45:43 -0300

    feat: implements My Ads Recycler View and its navigation

--- app/src/main/java/com/example/roommate/ui/fragments/FragmentMyAds.kt
+++ app/src/main/java/com/example/roommate/ui/fragments/FragmentMyAds.kt
@@ -7,12 +7,16 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.roommate.R
+import com.example.roommate.data.model.AdModel
 import com.example.roommate.databinding.FragmentMyAdsBinding
+import com.example.roommate.ui.adapters.ListAdAdapter
 
 
 class FragmentMyAds : Fragment(R.layout.fragment_my_ads) {
     private lateinit var binding: FragmentMyAdsBinding
+    private lateinit var adapter: ListAdAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,7 +26,9 @@ class FragmentMyAds : Fragment(R.layout.fragment_my_ads) {
         super.onCreateView(inflater, container, savedInstanceState)
 
         binding = FragmentMyAdsBinding.inflate(inflater, container, false)
-
+        adapter = ListAdAdapter{
+            findNavController().navigate(R.id.action_fragmentMyAds_to_fragmentAdvertisement)
+        }
         return binding.root
     }
 
@@ -32,5 +38,13 @@ class FragmentMyAds : Fragment(R.layout.fragment_my_ads) {
         binding.myAdsBtn.setOnClickListener {
             findNavController().navigate(R.id.action_fragmentMyAds_to_fragmentCreateAd1)
         }
+
+        binding.recycleListAds.layoutManager = LinearLayoutManager(context)
+        binding.recycleListAds.adapter = adapter
+
+        // Apenas para testar o Recycle View
+        adapter.updateAdList(mutableListOf( AdModel("TESTE3", "Bela Aurora/ Cariacica", 250.0)))
+        adapter.insertAdList(AdModel("TESTE2", "Bela Aurora/ Cariacica", 500.0))
+        adapter.insertAdList(AdModel("TESTE1", "Bela Aurora/ Cariacica", 750.0))
     }
 }
