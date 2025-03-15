c5873700ad71e9b8c5e6a5a5918ce59472d7a01a
lcrodrigues0 <leticia.c.rodrigues0@gmail.com>
2025-03-15 17:35:14 -0300

    feat: add navigation to ad from myAds

--- app/src/main/java/com/example/roommate/ui/fragments/FragmentMyAds.kt
+++ app/src/main/java/com/example/roommate/ui/fragments/FragmentMyAds.kt
@@ -30,8 +30,10 @@ class FragmentMyAds : Fragment(R.layout.fragment_my_ads) {
         myAdsVM = ViewModelProvider(this)[MyAdsViewModel::class.java]
 
         binding = FragmentMyAdsBinding.inflate(inflater, container, false)
-        adapter = ListAdAdapter{
-            findNavController().navigate(R.id.action_fragmentMyAds_to_fragmentAdvertisement)
+
+        adapter = ListAdAdapter{adModel ->
+            val action = FragmentMyAdsDirections.actionFragmentMyAdsToFragmentAdvertisement(adModel)
+            findNavController().navigate(action)
         }
         return binding.root
     }
