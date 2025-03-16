2cddbcfe1520cf24a3cd5f7782ef719cc2390775
siqueiradaniel <danielsiqueira111@gmail.com>
2025-03-16 16:56:34 -0300

    feat: click on favorite icon

--- app/src/main/java/com/example/roommate/ui/fragments/FragmentFavoriteAds.kt
+++ app/src/main/java/com/example/roommate/ui/fragments/FragmentFavoriteAds.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -23,9 +24,16 @@ class FragmentFavoriteAds : Fragment(R.layout.fragment_favorite_ads) {
         super.onCreateView(inflater, container, savedInstanceState)
 
         binding = FragmentFavoriteAdsBinding.inflate(inflater, container, false)
-        adapter = ListAdAdapter {
-            findNavController().navigate(R.id.action_fragmentFavoriteAds_to_fragmentAdvertisement)
-        }
+
+        adapter = ListAdAdapter(
+            onClickItem = {
+                findNavController().navigate(R.id.action_fragmentFavoriteAds_to_fragmentAdvertisement)
+            },
+            onClickButton = { ad ->
+                Toast.makeText(requireContext(), "Clicou no botão do anúncio: ${ad.title}", Toast.LENGTH_SHORT).show()
+            }
+        )
+
 
         return binding.root
     }
