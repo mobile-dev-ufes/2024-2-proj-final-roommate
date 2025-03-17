4c226dc8f3a08d4b8ff0b6c6135ed23d616815f3
lcrodrigues0 <leticia.c.rodrigues0@gmail.com>
2025-03-17 02:55:16 -0300

    feat: doc repositories

--- app/src/main/java/com/example/roommate/data/repository/AdRepository.kt
+++ app/src/main/java/com/example/roommate/data/repository/AdRepository.kt
@@ -12,10 +12,28 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.QueryDocumentSnapshot
 import com.google.firebase.storage.storage
 
+/**
+ * Classe responsável por interagir com o banco de dados Firebase para realizar operações
+ * relacionadas aos anúncios.
+ *
+ * A classe [AdRepository] fornece métodos para realizar as seguintes operações:
+ * - Obter todos os anúncios ou anúncios específicos de um usuário.
+ * - Criar um novo anúncio.
+ * - Salvar fotos associadas ao anúncio.
+ * - Obter imagens de anúncios armazenadas no Firebase Storage.
+ *
+ * @property db Instância do Firestore para interagir com o banco de dados.
+ * @property st Instância do FirebaseStorage para interagir com o armazenamento de fotos.
+ */
 class AdRepository {
     private val db = FirebaseFirestore.getInstance()
     private var st = Firebase.storage
 
+    /**
+     * Recupera todos os anúncios armazenados no Firestore e os retorna através do [liveData].
+     *
+     * @param liveData O LiveData que será atualizado com a lista de anúncios recuperados.
+     */
     fun getAllAds(liveData: MutableLiveData<MutableList<AdModel>>) {
         val adList = mutableListOf<AdModel>()
 
@@ -23,15 +41,21 @@ class AdRepository {
             .get()
             .addOnSuccessListener { result ->
                 for (document in result) {
-                    adList.add(AdModel(document))
+                    adList.add(AdModel(document))  // Adiciona cada anúncio à lista
                 }
-                liveData.value = adList
+                liveData.value = adList // Atualiza o LiveData com a lista de anúncios
             }
             .addOnFailureListener { exception ->
-                Log.d(TAG, "Error getting Ad documents: ", exception)
+                Log.d("FIREBASE-ADS", "Error getting Ad documents: ", exception)
             }
     }
 
+    /**
+     * Recupera os anúncios de um usuário específico baseado no ID do usuário.
+     *
+     * @param userId O ID do usuário cujos anúncios devem ser recuperados.
+     * @param liveData O LiveData que será atualizado com a lista de anúncios do usuário.
+     */
     fun getAdsByUser(userId: String, liveData: MutableLiveData<MutableList<AdModel>>) {
         val adList = mutableListOf<AdModel>()
 
@@ -40,10 +64,11 @@ class AdRepository {
             .get()
             .addOnSuccessListener { result ->
                 for (document in result) {
-                    adList.add(AdModel(document))
+                    adList.add(AdModel(document)) // Adiciona os anúncios do usuário à lista
                     Log.d("FIREBASE-MYADS", "${document.id} => ${document.data}")
                 }
-                Log.d("TESTE", "hello")
+
+                // Atualiza o LiveData com os anúncios do usuário
                 liveData.value = adList
             }
             .addOnFailureListener { exception ->
@@ -51,19 +76,29 @@ class AdRepository {
             }
     }
 
+    /**
+     * Cria um novo anúncio no Firestore e armazena as fotos associadas ao anúncio no
+     * Firebase Storage.
+     * Atualiza o status da operação por meio de [status].
+     *
+     * @param ad O modelo do anúncio a ser criado.
+     * @param status O LiveData que será atualizado com o status da operação (sucesso ou falha).
+     */
     fun create(ad: AdModel,  status : MutableLiveData<statusEnum>){
+        // Cria uma referência para um novo documento
         val adRef = db.collection("advertisement").document()
 
-        ad.id = adRef.id
-        saveAssets(ad, status)
+        ad.id = adRef.id // Atribui o ID gerado pelo Firestore ao anúncio
+        saveAssets(ad, status) // Salva as fotos do anúncio no Firebase Storage
 
-        val adMap = ad.toMap()
+        val adMap = ad.toMap() // Converte o anúncio para um mapa de dados
 
+        // Cria o documento no Firestore
         adRef.set(adMap)
             .addOnSuccessListener { documentReference ->
                 Log.d("FIREBASE-ADS", "Documento adicionado com ID: ${adRef.id}")
 
-                // Update the document with its generated ID
+                // // Atualiza o documento com seu ID gerado
                 adRef.update("id", adRef.id)
                     .addOnSuccessListener {
                         Log.d("FIREBASE-ADS", "ID atualizado com sucesso")
@@ -80,76 +115,56 @@ class AdRepository {
             }
     }
 
+    /**
+     * Salva as fotos associadas ao anúncio no Firebase Storage.
+     *
+     * @param ad O modelo de anúncio contendo as fotos.
+     * @param liveStatus O LiveData que será atualizado com o status da operação de upload das fotos.
+     */
     private fun saveAssets(ad: AdModel, liveStatus: MutableLiveData<statusEnum>){
+        // Retorna caso não haja fotos
         if (ad.photos.isEmpty()){
             liveStatus.value = statusEnum.SUCCESS
             return
         }
 
-        // Create a storage reference from our app
-        val storageRef = st.reference
 
+        val storageRef = st.reference  // Referência ao Firebase Storage
         val photos: MutableList<String> = mutableListOf()
 
         for (photo in ad.photos){
-            val file = Uri.parse(photo)
-            val path = "ads/${ad.id}/${file.lastPathSegment}"
-            val ref = storageRef.child(path)
+            val file = Uri.parse(photo) // Converte o caminho da foto para um URI
+            val path = "ads/${ad.id}/${file.lastPathSegment}" // Define o caminho no Firebase Storage
+            val ref = storageRef.child(path) // Cria uma referência para o arquivo
 
-            val uploadTask = ref.putFile(file)
+            val uploadTask = ref.putFile(file) // Realiza o upload da foto
 
-            photos.add("gs://${ref.bucket}/$path")
+            photos.add("gs://${ref.bucket}/$path") // Adiciona o caminho no storage à lista de fotos
 
             uploadTask
+                // Caso o upload seja bem-sucedido, o status é sucesso
                 .addOnSuccessListener {
                     liveStatus.value = statusEnum.SUCCESS
                     Log.d("FIRE - STORAGE", "Upload bem-sucedido: $file")
                 }
+                // Caso falhe, o status é de falha no upload da imagem
                 .addOnFailureListener { exception ->
                     liveStatus.value = statusEnum.FAIL_IMG
                     Log.d("FIRE - STORAGE", "Falha no upload: $file", exception)
                 }
         }
 
+        // Atualiza o modelo de anúncio com os caminhos das fotos no Firebase Storage
         ad.photos = photos
     }
 
-    // Substituída pelo construtor definido na classe
-    private fun adModelFromDocument(document: QueryDocumentSnapshot): AdModel{
-        var benefits = document.get("benefits")
-        var local = document.get("local")
-
-        benefits = if (benefits is Map<*, *>){
-            benefits as? Map<String, Boolean>
-        } else {
-            null
-        }
-
-        local = if(local is Map<*, *>) {
-            Address(local as Map<Any, Any>)
-        } else {
-            Address()
-        }
-
-        return AdModel(
-            id = document.getString("id"),
-            owner = document.getString("owner"),
-            title = document.getString("title"),
-            rent_value = document.getDouble("rent_value") ?: 0.0,
-            cond_value = document.getDouble("cond_value"),
-            max_client = document.getLong("max_client"),
-            description = document.getString("description"),
-            suite_qtt = document.getLong("suite_qtt"),
-            bedroom_qtt = document.getLong("bedroom_qtt"),
-            parking_qtt = document.getLong("parking_qtt"),
-            area = document.getDouble("area"),
-            benefits = benefits,
-            local = local,
-            groups = arrayOf()
-        )
-    }
-
-
+    /**
+     * Recupera a imagem de um anúncio armazenado no Firebase Storage.
+     *
+     * @param adId O ID do anúncio.
+     * @param onSuccess Função de callback chamada quando a URL da imagem é obtida com sucesso.
+     * @param onFailure Função de callback chamada quando ocorre um erro ao obter a URL da imagem.
+     */
     fun getAdImage(
         adId: String,
         onSuccess: (String) -> Unit,
@@ -161,20 +176,25 @@ class AdRepository {
                 val photosList = document.get("photos") as? List<String> ?: emptyList()
 
                 if (photosList.isNotEmpty()) {
-                    val firstPhotoUri = photosList.first()
-                    println("PHOTOSURI: $firstPhotoUri")
-                    getStorageUri(firstPhotoUri, onSuccess, onFailure)
+                    val firstPhotoUri = photosList.first() // Pega a primeira foto do anúncio
+                    getStorageUri(firstPhotoUri, onSuccess, onFailure)  // Recupera a URL da foto no Firebase Storage
                 } else {
-                    println("No photos available")
-                    onFailure(Exception("No photos found"))
+                    onFailure(Exception("No photos found")) // Caso não haja fotos, chama o callback de erro
                 }
             }
-
+            // Caso falhe ao recuperar os dados do documento, chama o callback de erro
             .addOnFailureListener { exception ->
                 onFailure(exception)
             }
     }
 
+    /**
+     * Recupera a URL de um arquivo de imagem no Firebase Storage a partir de seu URI.
+     *
+     * @param photoUri O URI da foto no Firebase Storage.
+     * @param onSuccess Função de callback chamada quando a URL do arquivo é obtida com sucesso.
+     * @param onFailure Função de callback chamada quando ocorre um erro ao obter a URL.
+     */
     private fun getStorageUri(
         photoUri: String,
         onSuccess: (String) -> Unit,
@@ -183,9 +203,12 @@ class AdRepository {
         if (photoUri.isNotEmpty()) {
             val storageRef = st.getReferenceFromUrl(photoUri)
             storageRef.downloadUrl
+                // Retorna a URL da foto
                 .addOnSuccessListener { uri -> onSuccess(uri.toString()) }
+                // Chama o callback de erro caso falhe
                 .addOnFailureListener { exception -> onFailure(exception) }
         } else {
+            // Caso o URI da foto não seja válido
             onFailure(Exception("Photo URI not found"))
         }
     }
--- app/src/main/java/com/example/roommate/data/repository/UserRepository.kt
+++ app/src/main/java/com/example/roommate/data/repository/UserRepository.kt
@@ -16,27 +16,32 @@ import java.time.ZoneId
 import java.util.Date
 
 /**
- * Repository responsible for managing user data in Firebase Firestore and Firebase Storage.
+ * Repositório responsável por gerenciar os dados do usuário no Firebase Firestore e Firebase
+ * Storage.
  *
- * This repository contains methods for creating and retrieving user data, as well as saving the user's image files
- * in Firebase Storage.
+ * Esta classe contém métodos para criar e recuperar dados de usuários, bem como salvar arquivos
+ * de imagem do usuário no Firebase Storage.
+ *
+ * Além disso, ela implementa operações como associar grupos ao usuário, obter informações sobre
+ * os grupos do usuário, e fazer upload de fotos de perfil no Firebase Storage.
+ *
+ * @constructor Cria uma instância do repositório de usuário, utilizando o Firestore e Firebase
+ * Storage.
  */
 class UserRepository {
-    // Firestore database instance
+    // Instância do Firestore
     private val db = FirebaseFirestore.getInstance()
-    private val storage = Firebase.storage
-
-    // Firebase Storage instance
+    // Instância do Firebase Storage
     private var st = Firebase.storage
 
     /**
-     * Creates a new user in Firestore and saves their data.
+     * Cria um novo usuário no Firestore e salva seus dados.
      *
-     * This method sends the user's information to Firestore and then uploads the user's photo
-     * to Firebase Storage, if a photo is provided.
+     * Este método envia as informações do usuário para o Firestore e, em seguida, faz o upload
+     * da foto do usuário para o Firebase Storage, caso uma foto seja fornecida.
      *
-     * @param user The user model containing the information to be saved.
-     * @param status LiveData for indicating the success or failure of the operation.
+     * @param user O modelo de usuário contendo as informações a serem salvas.
+     * @param status LiveData que indica o sucesso ou falha da operação.
      */
     fun create(user: UserModel, status : MutableLiveData<statusEnum>){
         saveAssets(user, status)
@@ -64,35 +69,38 @@ class UserRepository {
     }
 
     /**
-     * Saves the user's photo in Firebase Storage.
+     * Salva a foto do usuário no Firebase Storage.
      *
-     * If the user's photo is provided, this method uploads it to Firebase Storage. Otherwise,
-     * the status is marked as success without the need for upload.
+     * Caso a foto do usuário seja fornecida, este método faz o upload da foto para o Firebase Storage.
+     * Caso contrário, o status é marcado como sucesso sem necessidade de upload.
      *
-     * @param user The user model containing the photo to be saved.
-     * @param liveStatus LiveData that receives the status of the operation (success or failure).
+     * @param user O modelo de usuário contendo a foto a ser salva.
+     * @param liveStatus LiveData que recebe o status da operação (sucesso ou falha).
      */
     private fun saveAssets(user: UserModel, liveStatus: MutableLiveData<statusEnum>){
+        // Retorna caso não haja foto
         if (user.photo_uri == null){
             liveStatus.value = statusEnum.SUCCESS
             return
         }
 
-        // Create a storage reference from our app
         val storageRef = st.reference
 
+        // Prepara o caminho do arquivo no Storage
         val userId = user.email!!.replace(Regex("[^A-Za-z]"), "")
-
         val file = Uri.parse(user.photo_uri!!)
         val path = "users/$userId/${file.lastPathSegment}"
         val ref = storageRef.child(path)
 
+        // Inicia o upload do arquivo para o Firebase Storage.
         val uploadTask = ref.putFile(file)
 
         uploadTask
+            // Se o upload for bem-sucedido, marca o status como sucesso.
             .addOnSuccessListener {
                 liveStatus.value = statusEnum.SUCCESS
             }
+            // Se o upload falhar, marca o status como falha.
             .addOnFailureListener(){
                 Log.d("FIRESTORE", "get failed with $file")
                 liveStatus.value = statusEnum.FAIL_IMG
@@ -102,22 +110,25 @@ class UserRepository {
     }
 
     /**
-     * Retrieves user data from Firestore.
+     * Recupera os dados de um usuário a partir do Firestore.
      *
-     * This method fetches a user's information based on the provided email. If the user exists,
-     * their data is assigned to a `UserModel` object and returned via LiveData.
+     * Este método busca os dados de um usuário a partir do e-mail fornecido. Se o usuário existir,
+     * as informações são extraídas e retornadas através de um objeto `UserModel`, que é passado para
+     * o `LiveData` fornecido. Se o documento não for encontrado ou ocorrer um erro, um status de falha é retornado.
      *
-     * @param userEmail The email of the user to be fetched from Firestore.
-     * @param liveStatus LiveData that receives the status of the operation (success or failure).
-     * @param liveData LiveData that receives the `UserModel` object with the user's data.
+     * @param userEmail O e-mail do usuário a ser recuperado.
+     * @param liveStatus LiveData que recebe o status da operação (sucesso ou falha).
+     * @param liveData LiveData que recebe o objeto `UserModel` contendo os dados do usuário.
      */
     fun get(userEmail: String, liveStatus: MutableLiveData<statusEnum>, liveData: MutableLiveData<UserModel>) {
-        var user = UserModel()
+        var user: UserModel
 
+        // Realiza a busca no Firestore.
         db.collection("user").document(userEmail)
             .get()
             .addOnSuccessListener { document ->
                 if (document.exists()) {
+                    // Cria um objeto UserModel com os dados obtidos do Firestore.
                     user = UserModel(
                         document.getString("email"),
                         document.getString("name"),
@@ -129,19 +140,35 @@ class UserRepository {
                     )
                     Log.d("FIREBASE", "DocumentSnapshot data: ${document.data}")
 
+                    // Salva os dados e status no LiveData.
                     liveData.value = user
                     liveStatus.value = statusEnum.SUCCESS
+
+                // Se o documento não for encontrado, marca a operação como falha.
                 } else {
                     Log.d("FIREBASE", "No such document")
                     liveStatus.value = statusEnum.FAIL
                 }
             }
+            // Em caso de erro, marca a operação como falha.
             .addOnFailureListener { exception ->
                 Log.d("FIREBASE", "get failed with ", exception)
                 liveStatus.value = statusEnum.FAIL
             }
     }
 
+    /**
+     * Recupera os grupos aos quais um usuário pertence.
+     *
+     * Este método busca todos os grupos aos quais o usuário está associado. Ele coleta as
+     * referências dos grupos a partir do documento do usuário e, para cada grupo, recupera as
+     * informações detalhadas de cada grupo (nome, descrição, etc.) após o que retorna uma lista
+     * com os grupos do usuário. Um callback é chamado com essa lista de grupos quando a operação
+     * é concluída.
+     *
+     * @param userId O ID do usuário para o qual os grupos serão recuperados.
+     * @param callback Função de retorno que será chamada com a lista de grupos após a recuperação.
+     */
     fun getGroupsForUser(userId: String, callback: (List<GroupModel>) -> Unit) {
         val userRef = db.collection("user").document(userId)
 
@@ -151,6 +178,7 @@ class UserRepository {
             val groupList = mutableListOf<GroupModel>()
             var count = 0
 
+            // Recupera os detalhes de cada grupo associado ao usuário.
             groupRefs.forEach { groupRef ->
                 groupRef.get().addOnSuccessListener { groupDoc ->
                     groupDoc?.let {
@@ -167,33 +195,43 @@ class UserRepository {
                 }.addOnCompleteListener {
                     count++
                     if (count == groupRefs.size) {
-                        callback(groupList) // Call the callback with the list once all groups are fetched
+                        callback(groupList) // Chama o callback quando todos os grupos forem recuperados.
                     }
                 }
             }
         }.addOnFailureListener { e ->
             Log.w("GetGroups", "Error getting user document", e)
-            callback(emptyList()) // In case of error, return an empty list
+            callback(emptyList()) // Em caso de erro, retorna uma lista vazia
         }
     }
 
-
+    /**
+     * Adiciona um grupo à lista de grupos de um usuário.
+     *
+     * Este método recupera a lista de grupos associada ao usuário e adiciona a referência de
+     * um novo grupo.
+     * Se o grupo já estiver na lista do usuário, a operação é ignorada. Se o grupo não estiver
+     * presente, ele é adicionado à lista e a lista é atualizada no Firestore.
+     *
+     * @param userId O ID do usuário.
+     * @param groupId O ID do grupo a ser adicionado.
+     */
     fun addGroupToUser(userId: String, groupId: String) {
         val userRef = db.collection("user").document(userId)
         val groupRef = db.collection("group").document(groupId)
 
-        // Get the current list of groups for the user
+        // Recupera a lista atual de grupos do usuário.
         userRef.get().addOnSuccessListener { document ->
             val currentGroups = document.get("groups") as? MutableList<DocumentReference> ?: mutableListOf()
 
-            // Add the new group reference to the list if it's not already present
+            // Se o grupo não estiver na lista, adiciona o grupo.
             if (!currentGroups.contains(groupRef)) {
                 currentGroups.add(groupRef)
             } else {
                 println("User already a member of the this group")
             }
 
-            // Update the user's list of groups
+            // Atualiza a lista de grupos no Firestore.
             userRef.update("groups", currentGroups)
                 .addOnSuccessListener {
                     Log.d("AddGroupToUser", "Group $groupId added to user $userId")
@@ -206,6 +244,18 @@ class UserRepository {
         }
     }
 
+    /**
+     * Recupera a imagem de perfil de um usuário.
+     *
+     * Este método busca a URI da foto de perfil de um usuário armazenada no Firestore. Em seguida,
+     * utiliza o Firebase Storage para obter a URL pública do arquivo, que pode ser usada para exibir
+     * a imagem do perfil em interfaces de usuário.
+     *
+     * @param userId O ID do usuário.
+     * @param onSuccess Função de retorno chamada quando a URL da imagem de perfil é obtida com
+     * sucesso.
+     * @param onFailure Função de retorno chamada caso ocorra algum erro ao tentar obter a URL.
+     */
     fun getProfileImage(
         userId: String,
         onSuccess: (String) -> Unit,
@@ -222,6 +272,16 @@ class UserRepository {
             }
     }
 
+    /**
+     * Recupera a URL de download de um arquivo armazenado no Firebase Storage.
+     *
+     * Este método utiliza o URI da foto de perfil armazenada no Firebase Storage e retorna a URL
+     * pública do arquivo, para que possa ser utilizada para exibir a imagem ou outras operações.
+     *
+     * @param photoUri O URI do arquivo no Firebase Storage.
+     * @param onSuccess Função de retorno chamada quando a URL de download é obtida com sucesso.
+     * @param onFailure Função de retorno chamada caso ocorra algum erro durante o processo.
+     */
     private fun getStorageUri(
         photoUri: String,
         onSuccess: (String) -> Unit,
@@ -229,10 +289,14 @@ class UserRepository {
     ) {
         if (photoUri.isNotEmpty()) {
             val storageRef = st.getReferenceFromUrl(photoUri)
+            // Tenta obter a URL de download do arquivo armazenado
             storageRef.downloadUrl
+                // Em caso de sucesso, chama o callback onSuccess
                 .addOnSuccessListener { uri -> onSuccess(uri.toString()) }
+                // Em caso de falha, chama o callback onFailure com a exceção
                 .addOnFailureListener { exception -> onFailure(exception) }
         } else {
+            // Chama o callback onFailure com a exceção
             onFailure(Exception("Photo URI not found"))
         }
     }
