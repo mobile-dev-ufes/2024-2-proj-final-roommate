8a11e708729f3d67e4b803bc78bc1c8a399efd94
lcrodrigues0 <leticia.c.rodrigues0@gmail.com>
2025-03-17 00:02:33 -0300

    feat: doc utils

--- app/src/main/java/com/example/roommate/utils/authStatusEnum.kt
+++ app/src/main/java/com/example/roommate/utils/authStatusEnum.kt
@@ -1,12 +1,20 @@
 package com.example.roommate.utils
 
+/**
+ * Enumeração que representa os diferentes estados de autenticação.
+ *
+ * Cada valor desta enumeração descreve um possível estado de autenticação e estado gerado para cadastro no Firebase Auth durante o processo de login ou verificação de usuário.
+ *
+ * - [SUCCESS]: Indica que a autenticação foi bem-sucedida.
+ * - [WEAK_PASS]: Indica que a senha fornecida é considerada fraca.
+ * - [COLLISION]: Indica que houve um conflito (por exemplo, nome de usuário ou e-mail já existente).
+ * - [NETWORK]: Indica que ocorreu um erro relacionado à rede.
+ * - [FAIL]: Indica que a autenticação falhou, mas sem um motivo específico.
+ * - [NIL]: Indica que não há estado de autenticação definido ou o valor é nulo.
+ * - [INVALID_USER]: Indica que o usuário fornecido é inválido ou não existe na autenticação.
+ * - [INVALID_CREDENTIAL]: Indica que as credenciais fornecidas são inválidas.
+ */
+
 enum class authStatusEnum {
-    SUCCESS,
-    WEAK_PASS,
-    COLLISION,
-    NETWORK,
-    FAIL,
-    NIL,
-    INVALID_USER,
-    INVALID_CREDENTIAL
+    SUCCESS, WEAK_PASS, COLLISION, NETWORK, FAIL, NIL, INVALID_USER, INVALID_CREDENTIAL
 }
--- app/src/main/java/com/example/roommate/utils/userManager.kt
+++ app/src/main/java/com/example/roommate/utils/userManager.kt
@@ -2,6 +2,17 @@ package com.example.roommate.utils
 
 import com.example.roommate.data.model.UserModel
 
+/**
+ * Objeto singleton responsável por gerenciar o usuário atual na aplicação.
+ *
+ * Este objeto contém uma instância de [UserModel] que representa o usuário atual. Ele pode ser usado para armazenar
+ * ou acessar os dados do usuário durante o ciclo de vida da aplicação.
+ *
+ * A instância de [UserModel] é inicializada com valores padrão, mas pode ser alterada para refletir o usuário autenticado
+ * ou registrado no sistema.
+ *
+ * - [user]: Instância da classe [UserModel] que contém as informações do usuário, como nome, e-mail, etc.
+ */
 object userManager {
     var user = UserModel()
 }
