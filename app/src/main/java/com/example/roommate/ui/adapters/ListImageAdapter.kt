d80946eb18d6c9123467cfecbf53bab0f48cf2c6
lcrodrigues0 <leticia.c.rodrigues0@gmail.com>
2025-03-17 01:45:37 -0300

    feat: doc adapaters

--- app/src/main/java/com/example/roommate/ui/adapters/ListImageAdapter.kt
+++ app/src/main/java/com/example/roommate/ui/adapters/ListImageAdapter.kt
@@ -1,4 +1,9 @@
 package com.example.roommate.ui.adapters
 
+/**
+ * Adaptador responsável por exibir a lista de imagens no RecyclerView do anúncio.
+ *
+ * Será feito em implementações futuras
+ */
 class ListImageAdapter {
 }
